2439cee7fd2ce1e9235f8b7558d086a7606b7263
Rolando Chavarria <>
2021-04-15 21:16:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/* 4. Mostrar en pantalla la cantidad de segundos que tiene un lustro. 
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+ float n1;
+ n1 = 1.577e+8;
+    cout << "\nMuestra en pantalla la cantidad de segundos que tiene un lustro:\n ";//función del Prográma
+    cout << "\nLa cantidad de segundos que tiene un lustro es: "<<n1 <<" seg\n";
+    return 0;
+}
